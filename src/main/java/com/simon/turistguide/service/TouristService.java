5c09cc0bc8950e9978d8605ed2f6467e0c54ff06
Joshua-kea <josv0002@stud.kea.dk>
2025-08-27 10:26:50 +0200

    Tilføjet Service

--- src/main/java/com/simon/turistguide/service/TouristService.java
+++ src/main/java/com/simon/turistguide/service/TouristService.java
@@ -1,4 +1,19 @@
 package com.simon.turistguide.service;
 
+import com.simon.turistguide.model.TouristAttraction;
+import com.simon.turistguide.repository.TouristRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class TouristService {
+    private final TouristRespository repository;
+
+    public TouristService(TouristRespository repository) {
+        this.repository = repository;
+    }
+    public ArrayList<TouristAttraction> getTouristAttractions() {
+        return repository.getTouristAttractions();
+    }
 }
