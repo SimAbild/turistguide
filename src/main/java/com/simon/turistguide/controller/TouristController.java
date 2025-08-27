dbed40ee78fd0e84e3ebfd35b57dd8ea08546b59
MU-1995 <jynke58@hotmail.com>
2025-08-27 10:45:36 +0200

    TouristController added

--- src/main/java/com/simon/turistguide/controller/TouristController.java
+++ src/main/java/com/simon/turistguide/controller/TouristController.java
@@ -1,4 +1,27 @@
 package com.simon.turistguide.controller;
 
+import com.simon.turistguide.model.TouristAttraction;
+import com.simon.turistguide.service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("Attractions")
 public class TouristController {
+    private final TouristService touristService;
+
+    public TouristController(TouristService touristService){
+        this.touristService = touristService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ArrayList<TouristAttraction>> getAttractions() {
+        ArrayList<TouristAttraction> attractions = touristService.getTouristAttractions();
+        return new ResponseEntity<>(attractions,HttpStatus.OK);
+    }
 }
